e1a2ec36038c9d6b916bdf4ea6023f903d36362c
JorgeJOcampo <caramelos1>
2018-01-08 21:32:27 -0300

    Empieza la wea

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+
+mongoose.connect('mongodb://localhost:27017/hunger-games', (err, res) =>{
+    if(err){
+        console.log("Hay un problema con la base de datos: " +err);
+        throw err;
+    }else{
+        console.log("La base de datos está OK");
+app.listen(port, () => console.log("Iniciado en http://localhost:"+port));
+}
+});
